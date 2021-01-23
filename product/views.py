12e27bd281c93ec1032f54c63cc2b02c6d1faa28
Ammarat <acuster91@mail.ru>
2021-01-23 14:43:45 +0600

    right

--- product/views.py
+++ product/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render,HttpResponse
+from .models import ToDo
 
 def dz(request):
     return render(request, "index.html")
 
 def add12(request):
-    return render(request, "add12.html")
+    todo_list = ToDo.objects.all()
+    return render(request, "add12.html", {"todo_list": todo_list})
 
 
 def change12(request):
